affe45eb8a04b9def399a94cc78c8c0d2f8e9612
asrbdev <94693383+asrbdev@users.noreply.github.com>
2021-11-19 15:41:17 +0000

    Create PrintArgs.java
    Initial commit

---
+++ PrintArgs.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package exo1;
+
+/**
+ * @author malez
+ *
+ */
+public class PrintArgs {
+
+	/**
+	 * @param args
+	 */
+//	//Solution 1
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("TP2 Java");
+		for (String s : args) {
+			System.out.println(s);
+		}
+
+	}
+//
+//	//Solution 2
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		//System.out.println("TP2 Java");
+//		for (int i=0; i < args.length; i++) {
+//			System.out.println(args[i]);
+//		}
+//
+//	}
+
+}
